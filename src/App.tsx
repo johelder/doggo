4e1e3d07f1499e96096eca8d4fbcaadf011f6e3b
johelder <johelder.araujo@pede.ai>
2023-01-23 20:47:54 -0300

    feat: create folders structure

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View, SafeAreaView} from 'react-native';
+
+export function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello World</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
