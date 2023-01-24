41915eba625e50082c99ecc3fce92df662bd2291
johelder <johelder.araujo@pede.ai>
2023-01-23 22:23:55 -0300

    chore: configure absolute paths

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        alias: {
+          '@assets': './src/assets',
+          '@components': './src/components',
+        },
+      },
+    ],
+  ],
 };
