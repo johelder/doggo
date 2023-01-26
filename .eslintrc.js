26427b5d4935c7d6b4876bf5475aaddc0a54c846
johelder <johelder.araujo@pede.ai>
2023-01-26 20:08:14 -0300

    chore: disable no-unstable-nested-components rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,10 @@
 module.exports = {
   root: true,
   extends: ['@react-native-community', 'plugin:react-hooks/recommended'],
-  plugins: ['react-hooks', 'jsx-a11y'],
+  plugins: ['react-hooks'],
   rules: {
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
+    'react/no-unstable-nested-components': 'off',
   },
 };
