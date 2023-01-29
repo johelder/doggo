8c8d30a74e09f071ba7e2307e3ade7b87b8e58b5
johelder <johelder.araujo@pede.ai>
2023-01-29 16:36:39 -0300

    feat: display dynamic greetings at Home page

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
 
+import { useAuth } from '@src/hooks';
+import { getDayGreetings } from '@src/utils';
+
 import Logo from '@src/assets/images/logo.svg';
 
 import * as S from './styles';
 
 export function Home(): JSX.Element {
+  const { user } = useAuth();
+
   return (
     <S.Container>
       <S.HeaderContainer>
         <Logo />
 
         <S.HeaderContent>
-          <S.HeaderTitle>Bom dia, Johelder</S.HeaderTitle>
+          <S.HeaderTitle>
+            {getDayGreetings(new Date().getHours())}, {user?.displayName}
+          </S.HeaderTitle>
           <S.HeaderSubtitle>
             Falta pouco para ajudar animais de rua.
           </S.HeaderSubtitle>
