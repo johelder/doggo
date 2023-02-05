9ed91a2c65f9aae8532c21b9338e1862afcce39b
johelder <johelder.araujo@pede.ai>
2023-02-05 10:11:32 -0300

    refactor: removing unused screens

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,6 +1,4 @@
 import { SignIn } from './SignIn';
 import { Home } from './Home';
-import { Profile } from './Profile';
-import { Configurations } from './Configurations';
 
-export { SignIn, Home, Profile, Configurations };
+export { SignIn, Home };
