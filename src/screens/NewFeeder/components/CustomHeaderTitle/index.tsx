f6ba1865c6ca852b9cd2950affaff2246dd22dff
johelder <johelder.arj@gmail.com>
2023-05-21 15:23:19 -0300

    feat: create custom header title component

---
+++ src/screens/NewFeeder/components/CustomHeaderTitle/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { ICustomHeaderTitle } from './types';
+
+import * as S from './styles';
+
+export function CustomHeaderTitle({ title, subTitle }: ICustomHeaderTitle) {
+  return (
+    <S.Container>
+      <S.Title>{title}</S.Title>
+      <S.SubTitle>{subTitle}</S.SubTitle>
+    </S.Container>
+  );
+}
