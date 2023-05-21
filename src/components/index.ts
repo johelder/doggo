f4a9e07bcc283b3a7437886290373bef275449ce
johelder <johelder.arj@gmail.com>
2023-05-21 15:12:05 -0300

    feat: adding Loader to index components

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,6 @@ import { Button } from './Button';
 import { Map } from './Map';
 import { TabBarIcon } from './TabBarIcon';
 import { NavigationButton } from './NavigationButton';
+import { Loader } from './Loader';
 
-export { Button, Map, TabBarIcon, NavigationButton };
+export { Button, Map, TabBarIcon, NavigationButton, Loader };
