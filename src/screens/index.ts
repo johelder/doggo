4921abfafd1cef4e3b0126a40488358950c6e15c
johelder <johelder.arj@gmail.com>
2023-05-21 15:19:44 -0300

    feat: adding NewFeeder screen to index

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,5 +1,6 @@
 import { SignIn } from './SignIn';
 import { Home } from './Home';
 import { Profile } from './Profile';
+import { NewFeeder } from './NewFeeder';
 
-export { SignIn, Home, Profile };
+export { SignIn, Home, Profile, NewFeeder };
