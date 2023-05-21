089be07cc91231d3efde0d2d2aeb42157b259dee
johelder <johelder.arj@gmail.com>
2023-05-21 15:16:45 -0300

    refactor: adding header options

--- src/routes/authenticated/index.tsx
+++ src/routes/authenticated/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { SafeAreaView } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useTheme } from 'styled-components';
+
 import { NewFeeder } from '@src/screens/NewFeeder';
 import { HomeTabs } from './BottomTabs';
-import { SafeAreaView } from 'react-native';
+
 import { TRootStackParamList } from './types';
 
 const Stack = createNativeStackNavigator<TRootStackParamList>();
@@ -12,15 +15,25 @@ const wrapperOptions = {
 };
 
 export function AuthenticatedRoutes() {
+  const theme = useTheme();
+
   return (
     <SafeAreaView style={wrapperOptions}>
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          headerTintColor: theme.colors.primary[500],
+          headerTitleAlign: 'center',
+        }}>
         <Stack.Screen
           name="HomeTabs"
           component={HomeTabs}
           options={{ headerShown: false }}
         />
-        <Stack.Screen name="NewFeeder" component={NewFeeder} />
+        <Stack.Screen
+          name="NewFeeder"
+          component={NewFeeder}
+          options={{ headerTransparent: true }}
+        />
       </Stack.Navigator>
     </SafeAreaView>
   );
