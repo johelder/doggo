c465bd1ed7e2e2396f8ff83659f8546aeb7c07ce
johelder <johelder.arj@gmail.com>
2023-05-26 21:00:10 -0300

    feat: adding new components to index

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,7 @@ import { Map } from './Map';
 import { TabBarIcon } from './TabBarIcon';
 import { NavigationButton } from './NavigationButton';
 import { Loader } from './Loader';
+import { Input } from './Input';
+import { ChipButton } from './ChipButton';
 
-export { Button, Map, TabBarIcon, NavigationButton, Loader };
+export { Button, Map, TabBarIcon, NavigationButton, Loader, Input, ChipButton };
