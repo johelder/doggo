9dee1aea63663b272c81e91fa401da1e5a8590ba
johelder <johelder.arj@gmail.com>
2023-05-26 21:16:05 -0300

    feat: create Select Location screen

---
+++ src/screens/NewFeeder/screens/SelectLocation/styles.ts
@@ -0,0 +1,54 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.SafeAreaView``;
+
+export const CalloutContainer = styled.View`
+  margin-bottom: 14px;
+`;
+
+export const CalloutContent = styled.View`
+  background-color: ${({ theme }) => theme.colors.utils.white};
+  align-items: center;
+  padding: 16px 22px;
+
+  border-radius: 8px;
+`;
+
+export const CalloutTitle = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.primary.medium};
+  font-size: 12px;
+
+  color: ${({ theme }) => theme.colors.gray[700]};
+`;
+
+export const CalloutDescription = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.primary.regular};
+  font-size: 12px;
+  line-height: 16px;
+  text-align: center;
+
+  color: ${({ theme }) => theme.colors.gray[500]};
+`;
+
+export const ToolTipTriangle = styled.View`
+  width: 0;
+  height: 0;
+  border-left-width: 10px;
+  border-right-width: 10px;
+  border-top-width: 14px;
+  border-style: solid;
+  background-color: transparent;
+  border-left-color: transparent;
+  border-right-color: transparent;
+  border-top-color: ${({ theme }) => theme.colors.utils.white};
+  align-self: center;
+`;
+
+export const ButtonContainer = styled.View`
+  position: absolute;
+  bottom: 50px;
+  left: 0;
+  right: 0;
+
+  margin: 0 16px;
+`;
