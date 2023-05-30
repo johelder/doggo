91c5b73d8007c4da8e8050432368acbb42b930cb
johelder <johelder.arj@gmail.com>
2023-05-30 08:30:43 -0300

    feat: adding toast provider to app provider

--- src/providers/AppProvider/index.tsx
+++ src/providers/AppProvider/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import type { IAppProviderProps } from './types';
 
 import { AuthContextProvider, MapProvider } from '@src/hooks';
+import { ToastProvider } from '@src/components';
 
 import { ThemeProvider } from 'styled-components/native';
 import { theme } from '@src/styles/theme';
@@ -10,6 +11,7 @@ export function AppProvider({ children }: IAppProviderProps) {
   return (
     <ThemeProvider theme={theme}>
       <AuthContextProvider>
+        <ToastProvider />
         <MapProvider>{children}</MapProvider>
       </AuthContextProvider>
     </ThemeProvider>
