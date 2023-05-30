1b2634e7b81002cf6ef998fd41f5feb6cbafedf5
johelder <johelder.arj@gmail.com>
2023-05-30 08:28:08 -0300

    feat: create toast provider component

---
+++ src/components/ToastProvider/useToastProvider.ts
@@ -0,0 +1,29 @@
+import { useCallback, useEffect, useState } from 'react';
+
+import { toastEventManager } from '@src/utils/showToast';
+
+import { IToast } from './types';
+
+export function useToastProvider() {
+  const [toast, setToast] = useState<IToast | null>(null);
+
+  const handleAddToast = useCallback(({ type, message, duration }: IToast) => {
+    setToast({
+      type,
+      message,
+      duration,
+    });
+  }, []);
+
+  const handleRemoveToast = useCallback(() => {
+    setToast(null);
+  }, []);
+
+  useEffect(() => {
+    toastEventManager.on('addToast', handleAddToast);
+
+    return () => toastEventManager.removeListener('addToast', handleAddToast);
+  }, [handleAddToast]);
+
+  return { toast, handleRemoveToast };
+}
