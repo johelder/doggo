ccf440ffc3d9af332575ad0ca7f96e69b174b27d
johelder <johelder.arj@gmail.com>
2023-05-31 10:49:02 -0300

    feat: adding new screen to index screens

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -3,5 +3,6 @@ import { Home } from './Home';
 import { Profile } from './Profile';
 import { SelectLocation } from './NewFeeder/screens/SelectLocation';
 import { CreateFeeder } from './NewFeeder/screens/CreateFeeder';
+import { MyFeeders } from './MyFeeders';
 
-export { SignIn, Home, Profile, SelectLocation, CreateFeeder };
+export { SignIn, Home, Profile, SelectLocation, CreateFeeder, MyFeeders };
