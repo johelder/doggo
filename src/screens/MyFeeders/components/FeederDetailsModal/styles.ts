b904c10e7da773faaf6da5a0b6f4693038447dc6
johelder <johelder.arj@gmail.com>
2023-07-01 17:03:50 -0300

    feat: create feeder details modal

---
+++ src/screens/MyFeeders/components/FeederDetailsModal/styles.ts
@@ -0,0 +1,30 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  padding-bottom: 32px;
+`;
+
+export const Header = styled.View`
+  align-items: center;
+  margin-top: 16px;
+`;
+
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.primary.semiBold};
+  font-size: 14px;
+
+  color: ${({ theme }) => theme.colors.gray[700]};
+`;
+
+export const Description = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.primary.semiBold};
+  font-size: 10px;
+
+  color: ${({ theme }) => theme.colors.gray[500]};
+`;
+
+export const ActionsContainer = styled.View`
+  flex-direction: row;
+  gap: 10px;
+  margin: 32px 0 16px;
+`;
---
+++ src/screens/MyFeeders/components/FeederDetailsModal/types.ts
@@ -0,0 +1,11 @@
+import type { RefObject } from 'react';
+import type { Modalize } from 'react-native-modalize';
+import type { IDomainFeeder } from '@src/types/domain';
+
+export interface IFeederDetailsModal {
+  detailsModalRef: RefObject<Modalize>;
+  feeder: IDomainFeeder | null;
+  onCancel: () => void;
+  onDelete: (feederId: string) => Promise<void>;
+  isLoadingDelete: boolean;
+}
