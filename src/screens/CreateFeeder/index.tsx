9140548fc1362afad34a6811bd0611693fef73ee
johelder <johelder.arj@gmail.com>
2023-07-05 08:07:01 -0300

    refactor: using minimap component

--- src/screens/CreateFeeder/index.tsx
+++ src/screens/CreateFeeder/index.tsx
@@ -5,14 +5,12 @@ import {
   TouchableWithoutFeedback,
 } from 'react-native';
 import { useHeaderHeight } from '@react-navigation/elements';
-import { Marker } from 'react-native-maps';
 
 import { FeederForm } from '@src/components';
 import { CustomHeader } from '@src/components/CustomHeader';
+import { MiniMap } from '@src/components/Map/components/MiniMap';
 import { useCreateFeeder } from './useCreateFeeder';
 
-import { LATITUDE_DELTA, LONGITUDE_DELTA } from '@src/components/Map/constants';
-import { MAX_ZOOM_LEVEL, MIN_ZOOM_LEVEL } from './constants';
 import type { TCreateFeederProps } from './types';
 
 import * as S from './styles';
@@ -28,27 +26,7 @@ export function CreateFeeder({ route }: TCreateFeederProps): JSX.Element {
     <S.Container>
       <CustomHeader />
 
-      <S.MapPreview
-        initialRegion={{
-          latitude,
-          longitude,
-          latitudeDelta: LATITUDE_DELTA,
-          longitudeDelta: LONGITUDE_DELTA,
-        }}
-        style={{ marginTop: headerHeight }}
-        zoomEnabled={false}
-        zoomTapEnabled={false}
-        zoomControlEnabled={false}
-        scrollEnabled={false}
-        minZoomLevel={MIN_ZOOM_LEVEL}
-        maxZoomLevel={MAX_ZOOM_LEVEL}>
-        <Marker
-          coordinate={{
-            latitude,
-            longitude,
-          }}
-        />
-      </S.MapPreview>
+      <MiniMap coords={{ latitude, longitude }} headerHeight={headerHeight} />
 
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <KeyboardAvoidingView behavior="position">
