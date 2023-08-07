5d407f9f01221facd99df38e9bad80eb77c5c535
johelder <johelder.arj@gmail.com>
2023-08-07 08:10:49 -0300

    refactor: adjust constant name

--- src/utils/getDaysDifference.ts
+++ src/utils/getDaysDifference.ts
@@ -1,5 +1,7 @@
-const DAY_MILLISECONDS = 1000 * 60 * 60 * 24;
+const DAY_IN_MILLISECONDS = 1000 * 60 * 60 * 24;
 
 export function getDaysDifference(date: Date) {
-  return Math.round((date.getTime() - new Date().getTime()) / DAY_MILLISECONDS);
+  return Math.round(
+    (date.getTime() - new Date().getTime()) / DAY_IN_MILLISECONDS,
+  );
 }
