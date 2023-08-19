05c908f0fa69c2c0cbd1b762ced8fbb47048a7f1
johelder <johelder.arj@gmail.com>
2023-08-19 20:29:52 -0300

    feat: configure geolocation

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,12 @@ import { AppProvider } from '@src/providers';
 import { Routes } from '@src/routes';
 
 import { enableLatestRenderer } from 'react-native-maps';
+import Geolocation from '@react-native-community/geolocation';
 
 enableLatestRenderer();
+Geolocation.setRNConfiguration({
+  skipPermissionRequests: true,
+});
 
 export function App(): JSX.Element {
   return (
