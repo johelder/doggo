63fb2df6fa63b246bcd4b4703a8f234d57f9ae31
johelder <johelder.arj@gmail.com>
2023-08-27 10:22:07 -0300

    refactor: adjust app absolute paths

--- babel.config.js
+++ babel.config.js
@@ -4,14 +4,30 @@ module.exports = {
     [
       'module-resolver',
       {
-        root: ['./src'],
+        root: ['.'],
         alias: {
-          '*': '.',
-          '@src': './src',
+          '@app': './',
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@hooks': './src/hooks',
+          '@lib': './src/lib',
+          '@providers': './src/providers',
+          '@routes': './src/routes',
+          '@screens': './src/screens',
+          '@services': './src/services',
+          '@theme': './src/styles/theme',
+          '@types': './src/types',
+          '@utils': './src/utils',
         },
       },
     ],
-    'module:react-native-dotenv',
+    [
+      'module:react-native-dotenv',
+      {
+        ModuleName: '@env',
+        path: '.env',
+      },
+    ],
     'react-native-reanimated/plugin',
   ],
 };
