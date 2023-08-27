24af207a49c4961c32e10de677deb17d4372986c
johelder <johelder.arj@gmail.com>
2023-08-27 19:47:38 -0300

    fix: prop with wrong name

--- src/components/CustomHeader/components/CustomHeaderTitle/styles.ts
+++ src/components/CustomHeader/components/CustomHeaderTitle/styles.ts
@@ -2,11 +2,16 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   align-items: center;
+  justify-content: center;
 `;
 
 export const Title = styled.Text`
+  max-width: 300px;
+
   font-family: ${({ theme }) => theme.fonts.primary.medium};
   font-size: ${({ theme }) => theme.sizes.md}px;
+  line-height: ${({ theme }) => theme.spacings.xmd}px;
+  text-align: center;
 
   color: ${({ theme }) => theme.colors.gray[700]};
 `;
