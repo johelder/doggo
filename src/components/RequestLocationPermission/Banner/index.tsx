c5b26426c231bd17d2dbebd56402558aba22eda4
johelder <johelder.arj@gmail.com>
2023-09-07 10:40:48 -0300

    refactor: changing title to first

--- src/components/RequestLocationPermission/Banner/index.tsx
+++ src/components/RequestLocationPermission/Banner/index.tsx
@@ -7,14 +7,14 @@ import * as S from './styles';
 export function RequestLocationPermissionBanner() {
   return (
     <S.Container>
-      <LocationIllustration />
-
       <S.TextsContainer>
         <S.Title>Permitir localização</S.Title>
         <S.Description>
           Nós usamos sua localização para mostrar os comedouros próximos a você.
         </S.Description>
       </S.TextsContainer>
+
+      <LocationIllustration />
     </S.Container>
   );
 }
