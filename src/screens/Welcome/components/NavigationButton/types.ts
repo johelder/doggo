ab7fe52ca9dbc32a0c809505dafb2b77a709eb5d
johelder <johelder.arj@gmail.com>
2023-09-21 13:57:08 -0300

    fix: removing unused pages

--- src/screens/Welcome/components/NavigationButton/types.ts
+++
@@ -1,8 +0,0 @@
-import { ReactElement } from 'react';
-import { TouchableOpacityProps } from 'react-native';
-
-export interface INavigationButtonProps extends TouchableOpacityProps {
-  title: string;
-  description?: string;
-  icon: ReactElement;
-}
