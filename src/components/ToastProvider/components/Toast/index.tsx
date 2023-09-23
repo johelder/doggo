7b0ee5a47cdaa12e2756e2f71d5ef4a876858130
johelder <johelder.arj@gmail.com>
2023-09-23 11:18:30 -0300

    feat: implement swipe to dismiss on toast component

--- src/components/ToastProvider/components/Toast/index.tsx
+++ src/components/ToastProvider/components/Toast/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useTheme } from 'styled-components/native';
+import { GestureDetector } from 'react-native-gesture-handler';
 
 import { useToast } from './useToast';
 
@@ -8,28 +9,31 @@ import type { IToastProps } from './types';
 import * as S from './styles';
 
 export function Toast({ isVisible, toast, onRemove }: IToastProps) {
-  const { getToastIcon, animatedStyle, handleRemove } = useToast({
-    isVisible,
-    toast,
-    onRemove,
-  });
+  const { getToastIcon, animatedStyle, handleRemove, swipeUpGesture } =
+    useToast({
+      isVisible,
+      toast,
+      onRemove,
+    });
   const theme = useTheme();
 
   const Icon = getToastIcon(toast?.type);
 
   return (
-    <S.Container style={[animatedStyle]}>
-      <S.Content type={toast?.type}>
-        <S.IconContainer>
-          {Icon && <Icon color={theme.colors.gray[0]} />}
-        </S.IconContainer>
+    <GestureDetector gesture={swipeUpGesture}>
+      <S.Container style={[animatedStyle]}>
+        <S.Content type={toast?.type}>
+          <S.IconContainer>
+            {Icon && <Icon color={theme.colors.gray[0]} />}
+          </S.IconContainer>
 
-        <S.Message>{toast?.message}</S.Message>
+          <S.Message>{toast?.message}</S.Message>
 
-        <S.CloseButton onPress={handleRemove}>
-          <S.CloseIcon color={theme.colors.gray[0]} />
-        </S.CloseButton>
-      </S.Content>
-    </S.Container>
+          <S.CloseButton onPress={handleRemove}>
+            <S.CloseIcon color={theme.colors.gray[0]} />
+          </S.CloseButton>
+        </S.Content>
+      </S.Container>
+    </GestureDetector>
   );
 }
