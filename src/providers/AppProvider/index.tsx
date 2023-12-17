e2ad07e2a5da4151c89ca9026007097f979a551a
johelder <johelder.arj@gmail.com>
2023-12-17 12:20:36 -0300

    feat: wrapper app in query client provider

--- src/providers/AppProvider/index.tsx
+++ src/providers/AppProvider/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Host } from 'react-native-portalize';
 import { ThemeProvider } from 'styled-components/native';
@@ -10,20 +11,24 @@ import { theme } from '@theme';
 
 import { IAppProviderProps } from './types';
 
+const queryClient = new QueryClient();
+
 export function AppProvider({ children }: IAppProviderProps) {
   return (
     // eslint-disable-next-line react-native/no-inline-styles
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <ThemeProvider theme={theme}>
-        <Host>
-          <AuthContextProvider>
-            <FavoriteProvider>
-              <ToastProvider />
-              <MapProvider>{children}</MapProvider>
-            </FavoriteProvider>
-          </AuthContextProvider>
-        </Host>
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <Host>
+            <AuthContextProvider>
+              <FavoriteProvider>
+                <ToastProvider />
+                <MapProvider>{children}</MapProvider>
+              </FavoriteProvider>
+            </AuthContextProvider>
+          </Host>
+        </ThemeProvider>
+      </QueryClientProvider>
     </GestureHandlerRootView>
   );
 }
