21489f546e460cd4493e4a9060370c4a2035f124
johelder <johelder.arj@gmail.com>
2023-12-17 11:51:06 -0300

    feat: create useFeederFindNearest useCase

---
+++ src/domain/useCases/Feeder/useFeederFindNearest.ts
@@ -0,0 +1,49 @@
+import { FeederDomain } from '@data';
+import { calculateDistanceBetweenTwoPoints } from '@utils';
+
+export const THREE_KILOMETER_IN_METERS = 3000;
+
+export function useFeederFindNearest(location: any, feeders: FeederDomain[]) {
+  function filterFeedersInThreeKilometerRadio(allFeeders: FeederDomain[]) {
+    return allFeeders.filter(feeder => {
+      const distanceBetweenUserAndFeeder = calculateDistanceBetweenTwoPoints(
+        location.coords,
+        feeder.coordinates,
+      );
+
+      if (distanceBetweenUserAndFeeder < THREE_KILOMETER_IN_METERS) {
+        return feeder;
+      }
+    });
+  }
+
+  function sortFeedersByNearDistance(nearestFeeders: FeederDomain[]) {
+    return nearestFeeders.sort((a, b) => {
+      const firstDistance = calculateDistanceBetweenTwoPoints(
+        location.coords,
+        a.coordinates,
+      );
+
+      const secondDistance = calculateDistanceBetweenTwoPoints(
+        location.coords,
+        b.coordinates,
+      );
+
+      if (firstDistance < secondDistance) {
+        return -1;
+      }
+
+      return 1;
+    });
+  }
+
+  function getNearestFeeders(allFeeders: FeederDomain[]) {
+    return sortFeedersByNearDistance(
+      filterFeedersInThreeKilometerRadio(allFeeders),
+    ).slice(0, 10);
+  }
+
+  return {
+    nearFeeders: getNearestFeeders(feeders),
+  };
+}
