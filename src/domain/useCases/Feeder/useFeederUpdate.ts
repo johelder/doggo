f159c52406b21a02110106a2257aac3527231807
johelder <johelder.arj@gmail.com>
2023-12-20 22:58:32 -0300

    feat: create useFeederUpdate useCase

---
+++ src/domain/useCases/Feeder/useFeederUpdate.ts
@@ -0,0 +1,25 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+import { FeederDomain, FeederRepository } from '@data';
+import { MutationOptions, QueryKeys } from '@infrastructure';
+
+export function useFeederUpdate(options?: MutationOptions<FeederDomain>) {
+  const queryClient = useQueryClient();
+
+  const { mutate, isError, isPending } = useMutation<
+    void,
+    unknown,
+    FeederDomain
+  >({
+    mutationFn: feeder => FeederRepository.update(feeder),
+    onSuccess: () => options?.onSuccess?.(),
+    onError: () => options?.onError?.(),
+  });
+
+  function updateFeeder(feeder: FeederDomain) {
+    mutate(feeder);
+    queryClient.invalidateQueries({ queryKey: [QueryKeys.FeederList] });
+  }
+
+  return { updateFeeder, isError, isPending };
+}
