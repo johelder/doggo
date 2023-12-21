72aff67150f0b2a691505982a5efc6db17696793
johelder <johelder.arj@gmail.com>
2023-12-20 23:28:09 -0300

    fix: adding email to user type

--- src/data/models/domain/UserDomain.ts
+++ src/data/models/domain/UserDomain.ts
@@ -1,6 +1,7 @@
 export interface UserDomain {
   id: string;
   name: string | null;
+  email: string | null;
   photo: string;
   favorites: string[];
 }
--- src/data/models/persistence/UserPersistance.ts
+++ src/data/models/persistence/UserPersistance.ts
@@ -1,6 +1,7 @@
 export interface UserPersistance {
   id: string;
   name: string | null;
+  email: string | null;
   photo: string;
   favorites: string[];
 }
