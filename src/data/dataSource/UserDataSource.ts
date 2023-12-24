9ea0bb145e40ebb567247dbb9b8226fe4f69bd9f
johelder <johelder.arj@gmail.com>
2023-12-24 00:01:08 -0300

    feat: create findById, findFavoriteFeederById, addFavoriteFeeder, removeFavoriteFeeder and remove methods

--- src/data/dataSource/UserDataSource.ts
+++ src/data/dataSource/UserDataSource.ts
@@ -11,6 +11,71 @@ async function create(user: UserPersistance): Promise<void> {
     .set(user);
 }
 
+async function findById(id: string): Promise<UserPersistance | null> {
+  const user = await firestore()
+    .collection<UserPersistance>(FIRESTORE_USERS_COLLECTION)
+    .doc(id)
+    .get();
+
+  const data = user.data();
+
+  if (!data) {
+    return null;
+  }
+
+  return { ...data, id: user.id };
+}
+
+async function findFavoriteFeederById(
+  userId: string,
+  feederId: string,
+): Promise<string> {
+  const response = await firestore()
+    .collection<UserPersistance>(FIRESTORE_USERS_COLLECTION)
+    .doc(userId)
+    .get();
+
+  const favoritesList = response.data()?.favorites;
+
+  return favoritesList?.find(id => id === feederId) ?? '';
+}
+
+async function addFavoriteFeeder(
+  userId: string,
+  feederId: string,
+): Promise<void> {
+  firestore()
+    .collection<UserPersistance>(FIRESTORE_USERS_COLLECTION)
+    .doc(userId)
+    .update({
+      favorites: firestore.FieldValue.arrayUnion(feederId),
+    });
+}
+
+async function removeFavoriteFeeder(
+  userId: string,
+  feederId: string,
+): Promise<void> {
+  firestore()
+    .collection<UserPersistance>(FIRESTORE_USERS_COLLECTION)
+    .doc(userId)
+    .update({
+      favorites: firestore.FieldValue.arrayRemove(feederId),
+    });
+}
+
+async function remove(id: string): Promise<void> {
+  await firestore()
+    .collection<UserPersistance>(FIRESTORE_USERS_COLLECTION)
+    .doc(id)
+    .delete();
+}
+
 export const UserDataSource = {
   create,
+  findById,
+  findFavoriteFeederById,
+  addFavoriteFeeder,
+  removeFavoriteFeeder,
+  remove,
 };
