84666983b57ea5268bcdd94c311c8c6539850f70
johelder <johelder.arj@gmail.com>
2024-01-21 15:15:41 -0300

    fix: upstate error message

--- src/domain/useCases/User/useUserRemove.ts
+++ src/domain/useCases/User/useUserRemove.ts
@@ -5,11 +5,11 @@ import { MutationOptions } from '@infrastructure';
 import { errorHandler } from '@utils';
 
 export function useUserRemove(options?: MutationOptions<string>) {
-  const { mutate, isPending, isError } = useMutation<void, unknown, string>({
+  const { mutate, isPending, isError } = useMutation<void, Error, string>({
     mutationFn: id => UserRepository.remove(id),
     onSuccess: () => options?.onSuccess?.(),
     onError: error => {
-      options?.onError?.();
+      options?.onError?.(error.message);
       errorHandler.reportError(error, useUserRemove.name);
     },
   });
