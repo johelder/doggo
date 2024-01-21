4a07347b4a45e47c8bb876c206f8566cc509a9f7
johelder <johelder.arj@gmail.com>
2024-01-21 15:00:09 -0300

    test: adjust testing mocks

--- jest.config.ts
+++ jest.config.ts
@@ -4,12 +4,19 @@ const config: Config = {
   preset: 'react-native',
   setupFilesAfterEnv: ['<rootDir>/src/test/jest-setup.ts'],
   setupFiles: ['<rootDir>/src/test/jest-setup.ts'],
-  collectCoverageFrom: ['src/{components,utils,hooks}/**/*.{js,jsx,ts,tsx}'],
-  modulePathIgnorePatterns: ['.*/__mocks__/.*'],
+  collectCoverageFrom: [
+    'src/{components,utils,hooks,domain,screens}/**/*.{js,jsx,ts,tsx}',
+  ],
+  modulePathIgnorePatterns: ['.*/__mocks__/.*', 'e2e/'],
   moduleDirectories: ['node_modules', './src/test'],
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?|react-native-reanimated)/)',
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?|react-native-reanimated|@react-navigation)|react-native-map-clustering/)',
   ],
+
+  moduleNameMapper: {
+    '\\.svg':
+      '<rootDir>/src/test/__mocks__/external/react-native-svg-transformer.ts',
+  },
 };
 
 export default config;
