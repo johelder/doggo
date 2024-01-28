2659a7b62632e41c71a611e739bd052a88ee4ac3
johelder <johelder.arj@gmail.com>
2024-01-28 16:33:33 -0300

    feat: adding firebase emulators to development environment

--- src/data/dataSource/UserDataSource.ts
+++ src/data/dataSource/UserDataSource.ts
@@ -1,18 +1,18 @@
-import firestore from '@react-native-firebase/firestore';
+import database from '@react-native-firebase/firestore';
 
 import { UserPersistance } from '../models/persistence';
 
 import { FIRESTORE_USERS_COLLECTION } from './constants';
 
 async function create(user: UserPersistance): Promise<void> {
-  await firestore()
+  await database()
     .collection<UserPersistance>(FIRESTORE_USERS_COLLECTION)
     .doc(user.id)
     .set(user);
 }
 
 async function findById(id: string): Promise<UserPersistance | null> {
-  const user = await firestore()
+  const user = await database()
     .collection<UserPersistance>(FIRESTORE_USERS_COLLECTION)
     .doc(id)
     .get();
@@ -30,7 +30,7 @@ async function findFavoriteFeederById(
   userId: string,
   feederId: string,
 ): Promise<string> {
-  const response = await firestore()
+  const response = await database()
     .collection<UserPersistance>(FIRESTORE_USERS_COLLECTION)
     .doc(userId)
     .get();
@@ -44,11 +44,11 @@ async function addFavoriteFeeder(
   userId: string,
   feederId: string,
 ): Promise<void> {
-  firestore()
+  database()
     .collection<UserPersistance>(FIRESTORE_USERS_COLLECTION)
     .doc(userId)
     .update({
-      favorites: firestore.FieldValue.arrayUnion(feederId),
+      favorites: database.FieldValue.arrayUnion(feederId),
     });
 }
 
@@ -56,16 +56,16 @@ async function removeFavoriteFeeder(
   userId: string,
   feederId: string,
 ): Promise<void> {
-  firestore()
+  database()
     .collection<UserPersistance>(FIRESTORE_USERS_COLLECTION)
     .doc(userId)
     .update({
-      favorites: firestore.FieldValue.arrayRemove(feederId),
+      favorites: database.FieldValue.arrayRemove(feederId),
     });
 }
 
 async function remove(id: string): Promise<void> {
-  await firestore()
+  await database()
     .collection<UserPersistance>(FIRESTORE_USERS_COLLECTION)
     .doc(id)
     .delete();
