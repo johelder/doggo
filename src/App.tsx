44a177a3fe367c89bae5b0ee6aa3a81efe1a0298
johelder <johelder.arj@gmail.com>
2024-01-28 16:36:47 -0300

    feat: init firebase emulators at App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 
 import Geolocation from '@react-native-community/geolocation';
+import auth from '@react-native-firebase/auth';
+import firebase from '@react-native-firebase/firestore';
 import { enableLatestRenderer } from 'react-native-maps';
 
+import {
+  FIRESTORE_EMULATOR_HOST,
+  FIRESTORE_EMULATOR_PORT,
+  FIREBASE_AUTH_EMULATOR_URL,
+} from '@data';
 import { AppProvider } from '@providers';
 import { Routes } from '@routes';
 
@@ -10,6 +17,11 @@ enableLatestRenderer();
 Geolocation.setRNConfiguration({
   skipPermissionRequests: true,
 });
+
+if (__DEV__) {
+  firebase().useEmulator(FIRESTORE_EMULATOR_HOST, FIRESTORE_EMULATOR_PORT);
+  auth().useEmulator(FIREBASE_AUTH_EMULATOR_URL);
+}
 
 export function App(): React.JSX.Element {
   return (
