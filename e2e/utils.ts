21aa27ee09092a0058c5a130ba5e517deeab8ece
johelder <johelder.arj@gmail.com>
2024-01-28 16:35:56 -0300

    test: create function util to go back

--- e2e/utils.ts
+++ e2e/utils.ts
@@ -1,4 +1,4 @@
-import { by, element } from 'detox';
+import { by, element, device } from 'detox';
 import { AndroidElementAttributes, IosElementAttributes } from 'detox/detox';
 
 export async function getElementTextById(elementId: string) {
@@ -8,3 +8,15 @@ export async function getElementTextById(elementId: string) {
 
   return attributes.text ?? '';
 }
+
+export const goBack = async () => {
+  if (device.getPlatform() === 'android') {
+    await device.pressBack();
+
+    return;
+  }
+
+  await element(by.traits(['button']))
+    .atIndex(0)
+    .tap();
+};
